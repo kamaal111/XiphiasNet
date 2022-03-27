341dd5572498fdbfa2ebd4103d447904c175c0ea
Kamaal <kamaal.f1@gmail.com>
2022-03-27 14:11:07 +0200

    Testing failing requests

--- Tests/XiphiasNetTests/XiphiasNetSpec.swift
+++ Tests/XiphiasNetTests/XiphiasNetSpec.swift
@@ -13,18 +13,19 @@ import Foundation
 final class XiphiasNetSpec: QuickSpec {
     override func spec() {
         describe("request") {
-            context("Make succesfull requests") {
-                let apiURL = URL(string: "https://kamaal.io")!
-                let configuration = URLSessionConfiguration.default
-                configuration.protocolClasses = [MockURLProtocol.self]
-                let urlSession = URLSession(configuration: configuration)
-                let networker = XiphiasNet(urlSession: urlSession)
+            let apiURL = URL(string: "https://kamaal.io")!
+            let configuration = URLSessionConfiguration.default
+            configuration.protocolClasses = [MockURLProtocol.self]
+            let urlSession = URLSession(configuration: configuration)
+            let networker = XiphiasNet(urlSession: urlSession)
 
+            context("Make succesfull requests") {
                 it("makes requests") {
+                    let statusCode = 200
                     MockURLProtocol.requestHandler = { request in
                         let response = HTTPURLResponse(
                             url: apiURL,
-                            statusCode: 200,
+                            statusCode: statusCode,
                             httpVersion: nil,
                             headerFields: nil)!
 
@@ -43,11 +44,48 @@ final class XiphiasNetSpec: QuickSpec {
                             .request(from: apiURL)
                         let response = try result.get()
                         expect(response.data) == MockResponse(message: "yes")
+                        expect(response.status) == statusCode
 
                         expectation.fulfill()
                     }
 
-                    self.wait(for: [expectation], timeout: 1.0)
+                    self.wait(for: [expectation], timeout: 2)
+                }
+            }
+
+            context("Make failing requests") {
+                it("fails requests") {
+                    let statusCode = 400
+                    let jsonString = """
+                    {
+                        "message": "oh nooooo!"
+                    }
+                    """
+
+                    MockURLProtocol.requestHandler = { request in
+                        let response = HTTPURLResponse(
+                            url: apiURL,
+                            statusCode: statusCode,
+                            httpVersion: nil,
+                            headerFields: nil)!
+
+
+                        let data = jsonString.data(using: .utf8)
+                        return (response, data)
+                    }
+
+                    let expectation = self.expectation(description: "Expectation")
+                    Task {
+                        let result: Result<Response<MockResponse>, XiphiasNet.Errors> = await networker
+                            .request(from: apiURL)
+                        expect { try result.get() }.to(throwError { error in
+                            let castedError = error as! XiphiasNet.Errors
+                            expect(castedError) == .responseError(message: jsonString, code: 400)
+                            expectation.fulfill()
+                        })
+                    }
+
+                    self.wait(for: [expectation], timeout: 2)
                 }
             }
         }
@@ -68,19 +106,21 @@ class MockURLProtocol: URLProtocol {
     override func startLoading() {
         let handler = MockURLProtocol.requestHandler!
 
+        let (response, data): (HTTPURLResponse, Data?)
         do {
-            let (response, data) = try handler(request)
-
-            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-
-            if let data = data {
-                client?.urlProtocol(self, didLoad: data)
-            }
-
-            client?.urlProtocolDidFinishLoading(self)
+            (response, data) = try handler(request)
         } catch {
             client?.urlProtocol(self, didFailWithError: error)
+            return
         }
+
+        client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+
+        if let data = data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+
+        client?.urlProtocolDidFinishLoading(self)
     }
 
     override func stopLoading() { }
